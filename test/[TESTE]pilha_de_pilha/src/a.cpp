410804ee96b41c5c60aa692bd39b4c64c37d9a29
bnatalha <natalia_eletro@hotmail.com>
2017-05-18 15:36:02 -0300

    Teste da pilha adicionados [tentativa 3]

---
+++ test/[TESTE]pilha_de_pilha/src/a.cpp
@@ -0,0 +1,19 @@
+#include "header.h"
+#include "class_a.h"
+
+
+// test static member declaration/instatiation
+
+int main(int argc, char const *argv[])
+{
+	
+	cout << "Iniciando Teste..." << endl;
+
+	A_class A;
+
+	A.print_my();
+
+
+	cout << "Fechando Teste..." << endl;
+	return 0;
+}
